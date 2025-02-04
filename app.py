c3cb1c61a5cbfa34d96bb00780cf63c97463f27b
Margoman89 <mfgonzalezg89@gmail.com>
2025-02-04 12:12:33 -0600

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import plotly.express as px
 import streamlit as st
 
 # Leer los datos
-car_data = pd.read_csv('/Users/mariafernandagonzalez/Desktop/App/App/vehicles_us.csv')
+car_data = pd.read_csv(''vehicles_us.csv)
 
 
 # Encabezado de la aplicación
